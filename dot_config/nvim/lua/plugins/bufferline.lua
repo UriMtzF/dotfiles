1d666923397842e2e184e1de6a590eae169a4b7a
Uri Martínez <39018208+UriMtzF@users.noreply.github.com>
2024-12-05 02:28:44 -0600

    Update nvim config

--- dot_config/nvim/lua/plugins/bufferline.lua
+++ dot_config/nvim/lua/plugins/bufferline.lua
@@ -2,6 +2,12 @@ return {
   {
     "akinsho/bufferline.nvim",
     event = "VeryLazy",
+    keys = {
+      { "<leader>bsd", "<Cmd>BufferLineSortByDirectory<CR>", desc = "Sort by directory" },
+      { "<leader>bse", "<Cmd>BufferLineSortByExtension<CR>", desc = "Sort by extension" },
+      { "<leader>bsr", "<Cmd>BufferLineSortByRelativeDirectory<CR>", desc = "Sort by relative directory" },
+      { "<leader>bst", "<Cmd>BufferLineSortByTabs<CR>", desc = "Sort by tabs" },
+    },
     opts = {
       options = {
         always_show_bufferline = true,
---
+++ dot_wezterm.lua
@@ -0,0 +1,8 @@
+local wezterm = require("wezterm")
+local config = {}
+
+config.font = wezterm.font("FiraCode Nerd Font Mono")
+config.font_size = 10
+config.color_scheme = "Dracula"
+
+return config
