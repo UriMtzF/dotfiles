7f228ab6f281df2bdcfadc1d16fd77a61ab95932
Uri Martínez <39018208+UriMtzF@users.noreply.github.com>
2024-12-17 11:54:18 -0600

    Update wezterm config

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -1,8 +1,18 @@
 local wezterm = require("wezterm")
+local act = wezterm.action
 local config = {}
 
 config.font = wezterm.font("FiraCode Nerd Font Mono")
 config.font_size = 10
 config.color_scheme = "Dracula"
+
+config.keys = {}
+for i = 1, 8 do
+	table.insert(config.keys, {
+		key = tostring(i),
+		mods = "CTRL|ALT",
+		action = act.ActivateTab(i - 1),
+	})
+end
 
 return config
