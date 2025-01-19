a9bd68a1b8c1c893930dd75ad3523a21bead89b3
Uri Martínez <39018208+UriMtzF@users.noreply.github.com>
2025-01-19 01:31:34 -0600

    Update .config/nvim/lazy-lock.json Update .config/nvim/lazyvim.json Update .config/nvim/lua/config/autocmds.lua Update .config/nvim/lua/config/options.lua Add .config/nvim/lua/plugins/which-key.lua

--- dot_config/nvim/lua/config/autocmds.lua
+++ dot_config/nvim/lua/config/autocmds.lua
@@ -2,13 +2,24 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 --
-vim.api.nvim_create_autocmd({
-  "BufNewFile",
-  "BufRead",
-}, {
-  pattern = "*.typ",
-  callback = function()
-    local buf = vim.api.nvim_get_current_buf()
-    vim.api.nvim_buf_set_option(buf, "filetype", "typst")
+
+vim.api.nvim_create_autocmd({ "BufReadPre", "BufNewFile" }, {
+  pattern = { "*.pdf", "*.png", "*.jpg", "*.jpeg", "*.gif", "*.webp" },
+  callback = function(args)
+    if vim.fn.executable("xdg-open") == 0 then
+      vim.notify("xdg-open isn't installed in the system", vim.log.levels.ERROR)
+      return
+    end
+
+    local filename = vim.fn.fnameescape(args.file)
+    local bufferline = require("bufferline")
+    vim.notify("Opening file with external editor", vim.log.levels.INFO)
+    vim.system({ "xdg-open", filename })
+    vim.schedule(function()
+      if bufferline and vim.api.nvim_buf_is_valid(args.buf) then
+        bufferline.cycle(-1)
+        vim.api.nvim_buf_delete(args.buf, { force = true })
+      end
+    end)
   end,
 })
