e162b51bc3af2625626c93715dff184aa770df49
Uri Martínez <39018208+UriMtzF@users.noreply.github.com>
2025-01-20 21:51:01 -0600

    Update .config/nvim/lazy-lock.json Update .config/nvim/lazyvim.json Update .config/nvim/lua/plugins/flutter.lua Update .config/nvim/lua/plugins/snippets.lua

--- dot_config/nvim/lua/plugins/flutter.lua
+++ dot_config/nvim/lua/plugins/flutter.lua
@@ -8,17 +8,26 @@ return {
     },
     config = function()
       require("flutter-tools").setup({
+        closing_tags = {
+          highlight = "Comment",
+          prefix = "//",
+          enabled = true,
+        },
         lsp = {
           color = {
             enabled = true,
           },
           settings = {
             showTodos = true,
+            completeFunctionCalls = true,
+            renameFilesWithClasses = "prompt",
             enableSnippets = true,
+            enableSdkFormatter = true,
             updateImportsOnRename = true,
           },
         },
       })
     end,
   },
+  { "dart-lang/dart-vim-plugin" },
 }
--- dot_config/nvim/lua/plugins/snippets.lua
+++ dot_config/nvim/lua/plugins/snippets.lua
@@ -1,9 +1,17 @@
 return {
+  -- {
+  --   "garymjr/nvim-snippets",
+  --   opts = {
+  --     friendly_snippets = true,
+  --   },
+  --   dependencies = { "rafamadriz/friendly-snippets" },
+  -- },
   {
-    "garymjr/nvim-snippets",
-    opts = {
-      friendly_snippets = true,
-    },
-    dependencies = { "rafamadriz/friendly-snippets" },
+    "L3MON4D3/LuaSnip",
+    config = function()
+      local luasnip = require("luasnip")
+      luasnip.filetype_extend("dart", { "flutter" })
+      luasnip.filetype_extend("javascript", { "html" })
+    end,
   },
 }
